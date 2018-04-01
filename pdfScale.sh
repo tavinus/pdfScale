f69b6518fc220a51acd775cf9d8947551eed58f9
Gustavo Arnosti Neves <tavinus@users.noreply.github.com>
2018-03-31 23:43:01 -0300

    Add --image-resolution option (bump v2.1.3)
    Just bump version so we know we have the new option

--- pdfScale.sh
+++ pdfScale.sh
@@ -12,7 +12,7 @@
 #         And: https://gist.github.com/MichaelJCole/86e4968dbfc13256228a
 
 
-VERSION="2.1.2"
+VERSION="2.1.3"
 
 
 ###################### EXTERNAL PROGRAMS #######################
